2e2478ce60767ffa09b39b5e0dcbf18dfeb1fea8
pamelagao <pamela.gao@hotmail.com>
2018-05-17 11:10:54 -0400

    inclass birthday lab

--- lab2.js
+++ lab2.js
@@ -1,18 +1,18 @@
 //enums
 var months;
 (function (months) {
-    months[months["January"] = 1] = "January";
-    months[months["February"] = 2] = "February";
-    months[months["March"] = 3] = "March";
-    months[months["April"] = 4] = "April";
-    months[months["May"] = 5] = "May";
-    months[months["June"] = 6] = "June";
-    months[months["July"] = 7] = "July";
-    months[months["August"] = 8] = "August";
-    months[months["September"] = 9] = "September";
-    months[months["Octtober"] = 10] = "Octtober";
-    months[months["Novenber"] = 11] = "Novenber";
-    months[months["Decenber"] = 12] = "Decenber";
+    months[months["January"] = 0] = "January";
+    months[months["February"] = 1] = "February";
+    months[months["March"] = 2] = "March";
+    months[months["April"] = 3] = "April";
+    months[months["May"] = 4] = "May";
+    months[months["June"] = 5] = "June";
+    months[months["July"] = 6] = "July";
+    months[months["August"] = 7] = "August";
+    months[months["September"] = 8] = "September";
+    months[months["Octtober"] = 9] = "Octtober";
+    months[months["Novenber"] = 10] = "Novenber";
+    months[months["Decenber"] = 11] = "Decenber";
 })(months || (months = {}));
 var days;
 (function (days) {
@@ -34,10 +34,12 @@ var days;
 //     samedi,
 //     dimanche
 // }
+/*
 console.log('Month enum: ');
 console.log(months);
 console.log('January: ' + months.January);
 console.log('Month 1: ' + months[1]);
+*/
 //can not set value by below
 //months.January = 2;
 // Math.PI = 11;
@@ -46,17 +48,60 @@ var pTodayDate = document.getElementById("p--today-date");
 //Today's day
 // let today = Date.now();
 var today = new Date();
-console.log(today);
+// console.log(today);
 //Today's Month
-console.log('Today\'s Month: ' + today.getMonth());
+// console.log('Today\'s Month: ' + today.getMonth());
 var todayMonth = months[today.getMonth()];
 //Today's day of week
-console.log('Today\'s day of week: ' + today.getDay());
+// console.log('Today\'s day of week: ' + today.getDay());
 var todayDayOfWeek = days[today.getDay()];
 //Today's day of month
-console.log('Today\'s day of month: ' + today.getDate());
+// console.log('Today\'s day of month: ' + today.getDate());
 //Today's year
-console.log('Today\'s day of year: ' + today.getFullYear());
+// console.log('Today\'s day of year: ' + today.getFullYear());
 //display today's date to page
 //inside {} is js, and outside {} is array
 pTodayDate.innerHTML = "Today is " + todayDayOfWeek + ", " + todayMonth + " " + today.getDate() + ", " + today.getFullYear();
+//html elements
+// let btn_birthday = document.getElementById("button--birthday");
+// let input_birthday = document.getElementById("input--date-picker");
+// let msg_birthday = document.getElementById("p--birthday-message");
+// btn_birthday.addEventListener('click', function () {
+// btn_birthday.onclick = function () {
+//     console.log(input_birthday.value);
+//     let birthday = input_birthday.value;
+//     getBirthday(birthday);
+// }
+// // if (btn_birthday.click()) {
+// //
+// //     // msg_birthday.innerHTML = `Your birthday will be on `;
+// // }
+//
+// function getBirthday (d) {
+//     let d = new Date(d);
+//     // console.log(days[d.getDay()]);
+//     let birthdayDayOfWeek : string = days[d.getDay()];
+//     let birthdayOfMonth : string = months[d.getMonth()];
+//     msg_birthday.innerHTML = `Your birthday will be on ${ birthdayDayOfWeek } ${ birthdayOfMonth } ${ d.getDate() }, ${ d.getFullYear() }.`;
+// }
+// Lee's method
+var userBirthday;
+var buttonBirthday = document.getElementById("button--birthday");
+var inputDatePicker = document.getElementById("input--date-picker");
+var pBirthdayMessage = document.getElementById("p--birthday-message");
+
+buttonBirthday.onclick = function () {
+    userBirthday = inputDatePicker.value;
+    // console.log(userBirthday);
+    var userBirthdayDate = new Date(userBirthday + "GMT-0400");
+    pBirthdayMessage.innerHTML = constructDateString(userBirthdayDate);
+};
+function constructDateString(inputDate) {
+    //return 'happy birthday if the user's birthday is today
+    if ((inputDate.getMonth() === today.getMonth()) && (inputDate.getDate() === today.getDate())) {
+        return "Happy Birthday!";
+    }
+    //return the day of the birthday of the current year
+    var currentYearBirthday = new Date(today.getFullYear + "-" + inputDate.getMonth() + "-" + inputDate.getDate());
+    return "Your birthday is on " + days[currentYearBirthday.getDay()] + " " + months[currentYearBirthday.getMonth()] + " " + currentYearBirthday.getDate() + ", " + currentYearBirthday.getFullYear();
+}
--- lab2.ts
+++ lab2.ts
@@ -1,6 +1,6 @@
 //enums
 enum months {
-    January = 1,    //set the first one by value, others will be automatically increase number
+    January,    //set the first one by value, others will be automatically increase number
     February,
     March,
     April,
@@ -34,10 +34,12 @@ enum days {
 //     dimanche
 // }
 
+/*
 console.log('Month enum: ');
 console.log(months);
 console.log('January: ' + months.January);
 console.log('Month 1: ' + months[1]);
+*/
 
 //can not set value by below
 //months.January = 2;
@@ -50,23 +52,78 @@ let pTodayDate = document.getElementById("p--today-date");
 //Today's day
 // let today = Date.now();
 let today : Date = new Date();
-console.log(today);
+// console.log(today);
 
 //Today's Month
-console.log('Today\'s Month: ' + today.getMonth());
+// console.log('Today\'s Month: ' + today.getMonth());
 let todayMonth : string = months[today.getMonth()];
 
 //Today's day of week
-console.log('Today\'s day of week: ' + today.getDay());
+// console.log('Today\'s day of week: ' + today.getDay());
 let todayDayOfWeek : string = days[today.getDay()];
 
 //Today's day of month
-console.log('Today\'s day of month: ' + today.getDate());
-
+// console.log('Today\'s day of month: ' + today.getDate());
 
 //Today's year
-console.log('Today\'s day of year: ' + today.getFullYear());
+// console.log('Today\'s day of year: ' + today.getFullYear());
 
 //display today's date to page
 //inside {} is js, and outside {} is array
 pTodayDate.innerHTML = `Today is ${ todayDayOfWeek }, ${ todayMonth } ${ today.getDate()}, ${ today.getFullYear()}`;
+
+
+
+
+//html elements
+// let btn_birthday = document.getElementById("button--birthday");
+// let input_birthday = document.getElementById("input--date-picker");
+// let msg_birthday = document.getElementById("p--birthday-message");
+
+// btn_birthday.addEventListener('click', function () {
+// btn_birthday.onclick = function () {
+//     console.log(input_birthday.value);
+//     let birthday = input_birthday.value;
+//     getBirthday(birthday);
+// }
+// // if (btn_birthday.click()) {
+// //
+// //     // msg_birthday.innerHTML = `Your birthday will be on `;
+// // }
+//
+// function getBirthday (d) {
+//     let d = new Date(d);
+//     // console.log(days[d.getDay()]);
+//     let birthdayDayOfWeek : string = days[d.getDay()];
+//     let birthdayOfMonth : string = months[d.getMonth()];
+//     msg_birthday.innerHTML = `Your birthday will be on ${ birthdayDayOfWeek } ${ birthdayOfMonth } ${ d.getDate() }, ${ d.getFullYear() }.`;
+// }
+
+
+// Lee's method
+let userBirthday : string;
+
+let buttonBirthday = document.getElementById("button--birthday");
+let inputDatePicker = document.getElementById("input--date-picker");
+let pBirthdayMessage = document.getElementById("p--birthday-message");
+
+buttonBirthday.onclick = function () {
+    userBirthday = inputDatePicker.value;
+    // console.log(userBirthday);
+    let userBirthdayDate : Date = new Date(userBirthday + "GMT-0400");
+    pBirthdayMessage.innerHTML = constructDateString(userBirthdayDate);
+};
+
+function constructDateString (inputDate : Date) : string {
+    //return 'happy birthday if the user's birthday is today
+    if ( (inputDate.getMonth() === today.getMonth()) && (inputDate.getDate() === today.getDate()) ) {
+        return `Happy Birthday!`;
+    }
+
+    //return the day of the birthday of the current year
+    let currentYearBirthday : Date = new Date(`${ today.getFullYear }-${ inputDate.getMonth() }-${ inputDate.getDate() }`);
+
+    return `Your birthday is on ${ days[currentYearBirthday.getDay()] } ${ months[currentYearBirthday.getMonth()] } ${ currentYearBirthday.getDate() }, ${ currentYearBirthday.getFullYear() }`;
+}
+
+
